0a65d0ec172de15472bace4a882256c46e3f1e60
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-03-10 10:19:55 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+ 
 
 function NavBar() {
   return (
@@ -17,6 +18,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About(){
+  return(
+      <div id="about">
+        <h2>About</h2>
+      </div>
+  )
+}
 
 
 function App() {
@@ -24,7 +32,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+
+function Home(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
